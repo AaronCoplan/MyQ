23521fc177005dd64d35db73f59a72d4c35ea601
AaronCoplan <afcoplan@gmail.com>
2018-12-10 12:24:17 -0500

    Setting up to do search.

--- app/src/main/java/org/myq/SearchActivity.kt
+++ app/src/main/java/org/myq/SearchActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class SearchActivity : AppCompatActivity() {
@@ -12,12 +14,28 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var songList: List<Song>
     private lateinit var noSearchResultsTextView: TextView
+    private lateinit var searchButton: Button
+    private lateinit var searchEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         noSearchResultsTextView = findViewById(R.id.emptySearchResultsTextView)
+        searchButton = findViewById(R.id.searchButton)
+        searchEditText = findViewById(R.id.searchEditText)
+
+        searchButton.setOnClickListener{
+            val searchText: String = searchEditText.text.toString()
+            searchButton.isEnabled = false
+            searchEditText.isEnabled = false
+
+            // do search
+
+            searchButton.isEnabled = true
+            searchEditText.isEnabled = true
+            renderSongList()
+        }
 
         songList = ArrayList()
 
