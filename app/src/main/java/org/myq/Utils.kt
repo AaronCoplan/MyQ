3dbece269f7175a80a7ee274dec3c40b71114f34
seamusmalley <seamalley@gmail.com>
2018-12-10 12:15:38 -0500

    added track to song conversion functions

--- app/src/main/java/org/myq/Utils.kt
+++ app/src/main/java/org/myq/Utils.kt
@@ -2,8 +2,26 @@ package org.myq
 
 import android.content.Context
 import android.widget.Toast
+import com.spotify.protocol.types.Track
+import kaaes.spotify.webapi.android.models.Track
 
 fun makeToast(msg: String, context: Context) {
     val toast = Toast.makeText(context, msg, Toast.LENGTH_LONG)
     toast.show()
+}
+
+fun trackToSong(track: com.spotify.protocol.types.Track) {
+    if (track != null) {
+        Song(track.name, track.artist, track.uri)
+    } else {
+        null
+    }
+}
+
+fun trackToSong(track: kaaes.spotify.webapi.android.models.Track) {
+    if (track != null) {
+        Song(track.name, track.artists[0].name, track.uri)
+    } else {
+        null
+    }
 }
